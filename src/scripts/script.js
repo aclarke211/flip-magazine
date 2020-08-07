93b00e152b9c4aa53309146db0eb33e53da373ac
Alex Clarke <Alex.Clarke@jdplc.com>
2020-08-07 21:59:37 +0100

    feat(magazine): :sparkles: use jQuery Booklet plugin to create magazine
    also added content to the magazine and configure it to be functional.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,13 @@
 $(function() {
-  $('#magazine').booklet();
+  $('.magazine').booklet(
+   {
+      width: 800,
+      height: 525,
+      closed: true,
+      covers: true,
+      autoCenter: true,
+      pagePadding: 0,
+      pageNumbers: false,
+    }
+  );
 });
